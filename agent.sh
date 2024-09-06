cff97c8e9d0295889aa3e9f5cbfaa9c6c8716cb6
Beo <Beo.unregion@gmail.com>
2024-09-06 15:25:37 +0700

    update: init agent

--- agent.sh
+++ agent.sh
@@ -13,14 +13,14 @@ PARAMETER=$1
 case $PARAMETER in
     init)
         echo "Init the docker agent..."
-        docker run -d 
-            -p 9001:9001 
-            --name portainer_agent 
-            --restart=always 
-            -v /var/run/docker.sock:/var/run/docker.sock 
-            -v /var/lib/docker/volumes:/var/lib/docker/volumes 
-            -v /:/host
-            -e AGENT_SECRET=whooimboutamakeanameformyselfyea  
+        docker run -d \
+            -p 9001:9001 \
+            --name portainer_agent \
+            --restart=always \
+            -v /var/run/docker.sock:/var/run/docker.sock \
+            -v /var/lib/docker/volumes:/var/lib/docker/volumes \
+            -v /:/host \
+            -e AGENT_SECRET=whooimboutamakeanameformyselfyea \ 
             portainer/agent:2.21.0
         ;;
     start)
